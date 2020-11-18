93deee0bed21b81a62793108281bd5442ef76d6a
NguyenLanAnh2299 <43206459+NguyenLanAnh2299@users.noreply.github.com>
2020-11-18 21:20:44 +0700

    update

--- Bai10_ListProduct.js
+++ Bai10_ListProduct.js
@@ -1,8 +1,8 @@
-function listProduct () {
+function listProduct (count) {
     var list = [];
     var date = new Date();
-    for ( var i = 1 ; i <=10  ; i++) {
-        var product = new Product (i, "list:" + i, i % 2 + 1, date, 50 - i, false);
+    for ( var i = 1 ; i < count  ; i++) {
+        var product = new Product (i, "list:" + i, i % 2, date, 10+ i);
         list.push(product);
     }
     return list;
